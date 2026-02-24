eeff365ef7e771beafcb4b52bcad41e67caa8876
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 13:23:41 +0530

    Update Firebase configuration in auth.js
    Updated Firebase configuration with new project details.

--- auth.js
+++ auth.js
@@ -3,11 +3,15 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } f
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
+  authDomain: "nest-mr-bio.firebaseapp.com",
+  databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nest-mr-bio",
+  storageBucket: "nest-mr-bio.firebasestorage.app",
+  messagingSenderId: "1051338751163",
+  appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
+  measurementId: "G-DXZD3SVH46"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
