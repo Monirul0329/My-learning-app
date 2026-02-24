d884ba84802ae38c277444bd472960e3cdd3040b
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 20:33:33 +0530

    Refactor firebase.js to initialize Firebase app
    Removed HTML structure and added Firebase initialization code.

--- firebase.js
+++ firebase.js
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Student Signup</title>
-</head>
-<body>
-    <h2>Student Registration</h2>
-    <form id="signupForm">
-        <input type="text" id="userName" placeholder="Full Name" required><br><br>
-        <input type="text" id="userCity" placeholder="City" required><br><br>
-        <input type="number" id="userPhone" placeholder="Phone Number" required><br><br>
-        <input type="email" id="userEmail" placeholder="Email" required><br><br>
-        <input type="password" id="userPassword" placeholder="Password" required><br><br>
-        <button type="submit">Signup</button>
-    </form>
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-    <script type="module" src="app.js"></script>
-</body>
-</html>
-    
+const firebaseConfig = {
+    apiKey: "AizasyDYW3wJTOurtrgQffR2DsQimqj__w6-4_s",
+    authDomain: "nest-mr-bio.firebaseapp.com",
+    databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nest-mr-bio",
+    storageBucket: "nest-mr-bio.firebasestorage.app",
+    messagingSenderId: "1051338751163",
+    appId: "1:1051338751163:web:fd94176e0ccb9b251fbd9e",
+    measurementId: "G-PEN8W68ESL"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
