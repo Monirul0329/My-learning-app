10b12c20b16a5aea42aba69090e8f530ad77c074
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 20:48:12 +0530

    Update student.js

--- student.js
+++ student.js
@@ -3,13 +3,16 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AizasyDYW3wJTOurtrgQffR2DsQimqj__w6-4_s",
+    apiKey: "AizasyDYW3wJTOurtrgQffR2DsQimqj__w6-4_s", // Eita khub bhalo kore check koro
     authDomain: "nest-mr-bio.firebaseapp.com",
+    databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "nest-mr-bio",
     storageBucket: "nest-mr-bio.firebasestorage.app",
     messagingSenderId: "1051338751163",
-    appId: "1:1051338751163:web:fd94176e0ccb9b251fbd9e"
+    appId: "1:1051338751163:web:fd94176e0ccb9b251fbd9e",
+    measurementId: "G-PEN8W68ESL"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
