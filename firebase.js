8ddade8f29cab989aaf9fa523c79a81afc7c6fcf
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 20:51:09 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,15 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AizasyDYW3wJTOurtrgQffR2DsQimqj__w6-4_s", // Eita khub bhalo kore check koro
+    apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
     authDomain: "nest-mr-bio.firebaseapp.com",
     databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "nest-mr-bio",
     storageBucket: "nest-mr-bio.firebasestorage.app",
     messagingSenderId: "1051338751163",
-    appId: "1:1051338751163:web:fd94176e0ccb9b251fbd9e",
-    measurementId: "G-PEN8W68ESL"
-};
+    appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
+    measurementId: "G-DXZD3SVH46"
+  };
 
 
 const app = initializeApp(firebaseConfig);
