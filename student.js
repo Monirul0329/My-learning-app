e13530faac6000a61878382c460c36805c66332b
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 21:04:48 +0530

    Update student.js

--- student.js
+++ student.js
@@ -3,15 +3,15 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
+    apiKey: "AizasyDYW3wJTOurtrgQffR2DsQimqj__w6-4_s", // apiKey eiki ache
     authDomain: "nest-mr-bio.firebaseapp.com",
-    databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "nest-mr-bio",
     storageBucket: "nest-mr-bio.firebasestorage.app",
     messagingSenderId: "1051338751163",
-    appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
-    measurementId: "G-DXZD3SVH46"
-  };
+    appId: "1:1051338751163:web:59e6f33e44b266b31fbd9e", // NOTUN ID
+    measurementId: "G-DXZD3SVH46"                       // NOTUN ID
+};
+
 
 
 const app = initializeApp(firebaseConfig);
