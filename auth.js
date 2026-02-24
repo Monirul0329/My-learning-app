dbadd498573f0b1c73c4dff9d3863cdf32bc73c0
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 13:26:50 +0530

    Refactor firebaseConfig formatting in auth.js
    Removed unnecessary whitespace and reformatted the firebaseConfig object.

--- auth.js
+++ auth.js
@@ -3,15 +3,16 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } f
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
-  authDomain: "nest-mr-bio.firebaseapp.com",
-  databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "nest-mr-bio",
-  storageBucket: "nest-mr-bio.firebasestorage.app",
-  messagingSenderId: "1051338751163",
-  appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
-  measurementId: "G-DXZD3SVH46"
-};
+    apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
+    authDomain: "nest-mr-bio.firebaseapp.com",
+    databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nest-mr-bio",
+    storageBucket: "nest-mr-bio.firebasestorage.app",
+    messagingSenderId: "1051338751163",
+    appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
+    measurementId: "G-DXZD3SVH46"
+  };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
