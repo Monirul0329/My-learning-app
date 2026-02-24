9655da372a0d755ad3f359cbb3a55a8ddbf5f392
Monirul0329 <monirulgen45@gmail.com>
2026-02-25 00:20:03 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlmQwV3IN_asZolPyaBLBb7L_RG0uriZM",
+  apiKey: "AIzaSyDlmQWV3IN_asZolPyaBLBb7L_RG0uriZM",
   authDomain: "mneet-f9bc7.firebaseapp.com",
   projectId: "mneet-f9bc7",
   storageBucket: "mneet-f9bc7.firebasestorage.app",
   messagingSenderId: "944379440196",
-  appId: "1:944379440196:web:9d26b632b3e778d247e011"
+  appId: "1:944379440196:web:9d26b632b3e778d247e011",
+  measurementId: "G-70T6K3DLGT"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
