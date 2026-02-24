8be7fb05ff8c240c5562d492f152a65732d750e8
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 18:55:10 +0530

    Implement role-based login functions in auth.js
    Added login functions for admin, teacher, and student roles with role-based redirection.

--- auth.js
+++ auth.js
@@ -56,3 +56,54 @@ onAuthStateChanged(auth, (user) => {
     window.location.href = "dashboard.html";
   }
 });
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { db } from "./firebase.js";
+
+window.adminLogin = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+  if (snap.exists() && snap.data().role === "admin") {
+    window.location.href = "admin.html";
+  } else {
+    alert("Not Admin");
+  }
+};
+
+window.teacherLogin = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+  if (snap.exists() && snap.data().role === "teacher") {
+    window.location.href = "teacher.html";
+  } else {
+    alert("Not Teacher");
+  }
+};
+
+window.studentLogin = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+  if (snap.exists() && snap.data().role === "student") {
+    window.location.href = "student.html";
+  } else {
+    alert("Not Student");
+  }
+};
