9bdc5bde1ae3b4f0a2b53adbe686e3e5916883d7
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 20:28:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Signup</title>
+</head>
+<body>
+    <h2>Student Registration</h2>
+    <form id="signupForm">
+        <input type="text" id="userName" placeholder="Full Name" required><br><br>
+        <input type="text" id="userCity" placeholder="City" required><br><br>
+        <input type="number" id="userPhone" placeholder="Phone Number" required><br><br>
+        <input type="email" id="userEmail" placeholder="Email" required><br><br>
+        <input type="password" id="userPassword" placeholder="Password" required><br><br>
+        <button type="submit">Signup</button>
+    </form>
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
-    authDomain: "nest-mr-bio.firebaseapp.com",
-    databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "nest-mr-bio",
-    storageBucket: "nest-mr-bio.firebasestorage.app",
-    messagingSenderId: "1051338751163",
-    appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
-    measurementId: "G-DXZD3SVH46"
-  };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+    <script type="module" src="app.js"></script>
+</body>
+</html>
+    
