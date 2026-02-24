9b02a18f62cd463086384b949ee18971543d6f3b
Monirul0329 <monirulgen45@gmail.com>
2026-02-24 14:58:07 +0530

    Update Firebase config with new project settings
    Updated Firebase configuration with new project details.

--- student.js
+++ student.js
@@ -2,12 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, doc, getDoc, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// 🔥 তোমার firebase config এখানে বসাও
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-};
+    apiKey: "AIzaSyDYw3wJTOurtrgQffR2DsQ1mqj__w6-4_s",
+    authDomain: "nest-mr-bio.firebaseapp.com",
+    databaseURL: "https://nest-mr-bio-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nest-mr-bio",
+    storageBucket: "nest-mr-bio.firebasestorage.app",
+    messagingSenderId: "1051338751163",
+    appId: "1:1051338751163:web:59e5f33e44b265b31fbd9e",
+    measurementId: "G-DXZD3SVH46"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
