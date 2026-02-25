7c35ff508f4e3ca19a26505d10ccc3e00ef2c408
Monirul0329 <monirulgen45@gmail.com>
2026-02-25 12:51:02 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDlmQWV3IN_asZolPyaBLBb7L_RG0uriZM",
+    authDomain: "mneet-f9bc7.firebaseapp.com",
+    projectId: "mneet-f9bc7",
+    storageBucket: "mneet-f9bc7.firebasestorage.app",
+    appId: "1:944379440196:web:9d26b632b3e778d247e011"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
